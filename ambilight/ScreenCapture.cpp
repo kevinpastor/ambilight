639cb4b14a677c54baf51bfb5daf19162250041a
KevinPastor <kevin.pastor@polymtl.ca>
2018-09-21 21:47:36 -0400

    Forgot to save file for last commit

--- ambilight/ScreenCapture.cpp
+++ ambilight/ScreenCapture.cpp
@@ -21,28 +21,12 @@ std::vector<Pixel> ScreenCapture::getScreenData()
 	bmi.biCompression = BI_RGB;
 	bmi.biSizeImage = 0;
 
-	//void * screenCaptureData = new unsigned char[screenWidth * screenHeight * 3];
-	//GetDIBits(hdcMem, hBitmap, 0, screenHeight, (unsigned char *) screenCaptureData, (BITMAPINFO*)&bmi, DIB_RGB_COLORS);
-	std::vector<Pixel> sickData(screenWidth * screenHeight * 3);
-	GetDIBits(hdcMem, hBitmap, 0, screenHeight, &sickData[0], (BITMAPINFO*)&bmi, DIB_RGB_COLORS);
+	std::vector<Pixel> screenCaptureData(screenWidth * screenHeight * 3);
+	GetDIBits(hdcMem, hBitmap, 0, screenHeight, &screenCaptureData[0], (BITMAPINFO*)&bmi, DIB_RGB_COLORS);
 
 	ReleaseDC(GetDesktopWindow(), hScreen);
 	DeleteDC(hdcMem);
 	DeleteObject(hBitmap);
 
-	
-
-	//Pixel * pixelArray = reinterpret_cast<Pixel *>(screenCaptureData);
-
-
-	std::cout << "ici" << std::endl;
-
-	//std::vector<Pixel> test((Pixel*)screenCaptureData, (Pixel *)screenCaptureData + 1555429);
-	std::cout << (unsigned)sickData[4050070].red << std::endl;
-	system("pause");
-	//std::vector<Pixel> screenCapture(pixelArray, pixelArray);
-	//screenCapture.insert(screenCapture.begin(), pixelArray, pixelArray + screenWidth * screenHeight * 3);
-	//screenCapture.assign(pixelArray, pixelArray + 1555428);
-	//return screenCapture;
-	return sickData;
+	return screenCaptureData;
 }
